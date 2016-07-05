42710afaab1e0f8cde5ec9db33d9ffaa1e6835a6
SantiagoLazzari <santiago.lazzari@mercadolibre.com>
2016-07-05 14:04:04 -0300

    WIP #3 - Added login controller and some refactor in clases (swift do not need any namespaces)

--- uteach/models/NavigationFlowManager/NavigationFlowManager.swift
+++ uteach/models/NavigationFlowManager/NavigationFlowManager.swift
@@ -11,11 +11,9 @@ import UIKit
 class UTNavigationFlowManager: NSObject {
     
     //MARK: - Atributes
-    
     var window: UIWindow?
     
     //MARK: - Init
-    
     init(withWindow window: UIWindow) {
         super.init()
         self.window = window
@@ -34,13 +32,15 @@ class UTNavigationFlowManager: NSObject {
     }
     
     //MARK: - Presentations
-    
     func presentSplashAnimated(animated: Bool) {
-        presentViewController(UTSplashViewController(), animated: true)
+        presentViewController(UTSplashViewController(), animated: animated)
+    }
+    
+    func presentLoginViewController(animated: Bool) {
+        presentViewController(LoginViewController(), animated: animated);
     }
     
     //MARK: - Utils
-    
     func presentViewController(viewController: UIViewController, animated:Bool) {
         self.presentViewController(viewController, animated: animated, completion: nil)
     }
